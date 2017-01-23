a63bfc06ed98a99f239b167bd8ad73ea0b059918
Lina Yi <lyi@planittesting.com>
2017-01-23 16:05:41 +1300

    Update test Report generator

--- TestReport/run.bat
+++ TestReport/run.bat
@@ -1,3 +1,17 @@
 ..\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --labels=On --output=TestResult.txt --result="TestResult.xml";format=nunit2 ..\BDDSpecflowDemo\bin\Debug\BDDSpecflowDemo.dll
-ConvertNUnit3TxtReportToNUnit2.bat
-runReport.bat
+
+@echo off
+
+call:DoReplace "=>" "*****" TestResult.txt TestResult1.txt
+exit /b
+
+:DoReplace
+echo ^(Get-Content "%3"^) ^| ForEach-Object { $_ -replace %1, %2 } ^| Set-Content %4>Rep.ps1
+Powershell.exe -executionpolicy ByPass -File Rep.ps1
+if exist Rep.ps1 del Rep.ps1
+echo Done
+
+del TestResult.txt
+rename TestResult1.txt TestResult.txt
+
+..\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport ..\BDDSpecflowDemo\BDDSpecflowDemo.csproj
