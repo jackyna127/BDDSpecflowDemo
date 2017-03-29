dcea40763460dc764c723e56b8e62002260247b7
Lina Yi <lyi@planittesting.com>
2017-03-30 10:50:07 +1300

    Update the bat file for jenkins

--- TestReport/RunAndGenerateReport-Jenkins.bat
+++ TestReport/RunAndGenerateReport-Jenkins.bat
@@ -1,24 +1,25 @@
 @echo off
+Set path=%1
+echo "path is"
+echo %path%
 echo "Execute test from Nunit.Console"
-
-%1\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --labels=On --output=%1\TestReport\TestResult.txt --result=%1\TestReport\TestResult.xml;format=%1\BDDSpecflowDemo\bin\Debug\BDDSpecflowDemo.dll
+%path%\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --labels=On --output=%path%\TestReport\TestResult.txt --result=%path%\TestReport\TestResult.xml;format=nunit2 %path%\BDDSpecflowDemo\bin\Debug\BDDSpecflowDemo.dll
 
 echo "Change Nunit 3 txt report to NUnit 2 txt report"
 
+call:DoReplace "=>" "*****" %path%\TestReport\TestResult.txt %path%\TestReport\TestResult1.txt
+
+echo "Generate Report"
+
+%path%\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport %path%\BDDSpecflowDemo\BDDSpecflowDemo.csproj
+
+
+:DoReplace
 echo ^(Get-Content "%3"^) ^| ForEach-Object { $_ -replace %1, %2 } ^| Set-Content %4>Rep.ps1
 Powershell.exe -executionpolicy ByPass -File Rep.ps1
 if exist Rep.ps1 del Rep.ps1
 echo Done
 
-
-del %1\TestReport\TestResult.txt
-rename %1\TestReport\TestResult1.txt %1\TestReport\TestResult.txt
-
-echo "Generate Report"
-
-%1\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport %1\BDDSpecflowDemo\BDDSpecflowDemo.csproj
-
-
-
-
+del %path%\TestReport\TestResult.txt
+rename %path%\TestReport\TestResult1.txt %path%\TestReport\TestResult.txt
 
