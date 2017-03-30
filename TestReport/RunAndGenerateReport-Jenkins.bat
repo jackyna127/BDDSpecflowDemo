c6f106784dded931231b598512dae97b999b6c1e
Lina Yi <lyi@planittesting.com>
2017-03-30 13:33:20 +1300

    Update generate html report scripts

--- TestReport/RunAndGenerateReport-Jenkins.bat
+++ TestReport/RunAndGenerateReport-Jenkins.bat
@@ -12,7 +12,7 @@ call:DoReplace "=>" "*****" %path%\TestReport\TestResult.txt %path%\TestReport\T
 
 echo "Generate Report"
 
-%path%\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport %path%\BDDSpecflowDemo\BDDSpecflowDemo.csproj /testResult:%path%\TestReport\TestResult.xml /out:%path%\TestReport\TestResult.html
+%path%\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport %path%\BDDSpecflowDemo\BDDSpecflowDemo.csproj /out:%path%\TestReport\TestResult.html
 
 
 :DoReplace
