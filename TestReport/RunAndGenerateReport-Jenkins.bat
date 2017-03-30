646781eb29cd328cff8c1858f668c8614bf32671
Lina Yi <lyi@planittesting.com>
2017-03-30 13:49:33 +1300

    Update report generate script

--- TestReport/RunAndGenerateReport-Jenkins.bat
+++ TestReport/RunAndGenerateReport-Jenkins.bat
@@ -10,17 +10,18 @@ echo "Change Nunit 3 txt report to NUnit 2 txt report"
 
 call:DoReplace "=>" "*****" %path%\TestReport\TestResult.txt %path%\TestReport\TestResult1.txt %powershell%
 
+del %path%\TestReport\TestResult.txt
+rename %path%\TestReport\TestResult1.txt %path%\TestReport\TestResult.txt
+
 echo "Generate Report"
 
 %path%\packages\SpecFlow.2.1.0\tools\specflow.exe nunitexecutionreport %path%\BDDSpecflowDemo\BDDSpecflowDemo.csproj /out:%path%\TestReport\TestResult.html
-
+echo "Generate Report Done"
 
 :DoReplace
 echo ^(Get-Content "%3"^) ^| ForEach-Object { $_ -replace %1, %2 } ^| Set-Content %4>Rep.ps1
-%5\powershell.exe -executionpolicy ByPass -File Rep.ps1
+%5 -executionpolicy ByPass -File Rep.ps1
 if exist Rep.ps1 del Rep.ps1
 echo Done
 
-del %path%\TestReport\TestResult.txt
-rename %path%\TestReport\TestResult1.txt %path%\TestReport\TestResult.txt
 
